47645b2152302c7b88a562c77cc87cae73c8348e
Baptiste Mispelon <bmispelon@gmail.com>
2013-11-25 13:46:44 +0100

    Fixed bug in RemoveParticipantView.

--- noyel/kdo/views/present.py
+++ noyel/kdo/views/present.py
@@ -135,7 +135,7 @@ class RemoveParticipantView(LoginRequiredMixin, NextMixin, MessageMixin, generic
         self.present = get_object_or_404(Present, pk=kwargs['pk'],
                                          participants__user=request.user)
         self.user = get_object_or_404(User, pk=kwargs['user_pk'],
-                                      present=self.present)
+                                      participant__present=self.present)
         self.present.participants.filter(user=self.user).delete()
         msg = _("The user has been removed from the present's participants "
                 "successfully.")
